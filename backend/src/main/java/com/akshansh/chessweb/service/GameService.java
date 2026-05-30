f0d560edd532c4aee6a7d3766a94139ffe3ae25e
Akshansh <akshanshsingh00@gmail.com>
2026-05-30 16:11:29 +0530

    Added GameService and utils

--- backend/src/main/java/com/akshansh/chessweb/service/GameService.java
+++ backend/src/main/java/com/akshansh/chessweb/service/GameService.java
@@ -1,11 +1,38 @@
 package com.akshansh.chessweb.service;
 
+import com.akshansh.chessweb.model.GameSession;
+import com.akshansh.chessweb.model.dto.CreateGameReqDto;
+import com.akshansh.chessweb.model.enums.Color;
+import com.akshansh.chessweb.model.enums.GameStatus;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.UUID;
+
+import static com.akshansh.chessweb.utils.ChessConstants.STARTING_FEN;
 
 @Service
 @RequiredArgsConstructor
 public class GameService {
 
     private final GameStore store;
+
+    public UUID createGame(CreateGameReqDto request){
+        GameSession newSession = GameSession.builder()
+                .id(UUID.randomUUID())
+                .whitePlayerId(request.getPlayerColor().equals(Color.WHITE) ? request.getPlayerId() : null)
+                .blackPlayerId(request.getPlayerColor().equals(Color.BLACK) ? request.getPlayerId() : null)
+                .whitePlayerName(request.getPlayerColor().equals(Color.WHITE) ? request.getPlayerName() : null)
+                .blackPlayerName(request.getPlayerColor().equals(Color.BLACK) ? request.getPlayerName() : null)
+                .moveHistory(new ArrayList<>())
+                .currentTurn(Color.WHITE)
+                .currentFen(STARTING_FEN)
+                .status(GameStatus.WAITING)
+                .build();
+
+        store.saveGame(newSession);
+
+        return newSession.getId();
+    }
 }
---
+++ backend/src/main/java/com/akshansh/chessweb/utils/ChessConstants.java
@@ -0,0 +1,7 @@
+package com.akshansh.chessweb.utils;
+
+public final class ChessConstants {
+    private ChessConstants() {}
+
+    public static final String STARTING_FEN = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
+}
